160e91a1df74b0a4036c1f39792a5a92609c681d
EscapeFromJava <reske.artur@gmail.com>
2021-12-30 15:48:28 +0300

    Добавьте файлы проекта.

---
+++ CSharp_Work15_PainterWithForms/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace CSharp_Work15_PainterWithForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxMain = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.buttonLoadFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 460);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(576, 628);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(185, 23);
+            this.ClearButton.TabIndex = 6;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(576, 497);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(116, 15);
+            this.labelResult.TabIndex = 14;
+            this.labelResult.Text = "Свободных клеток: ";
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.Location = new System.Drawing.Point(304, 494);
+            this.textBoxMain.Multiline = true;
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.Size = new System.Drawing.Size(185, 118);
+            this.textBoxMain.TabIndex = 4;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(304, 628);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(185, 23);
+            this.buttonEnter.TabIndex = 5;
+            this.buttonEnter.Text = "Выполнить";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(93, 534);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(79, 19);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Вариант 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(93, 559);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(79, 19);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Вариант 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // buttonLoadFile
+            // 
+            this.buttonLoadFile.Location = new System.Drawing.Point(43, 628);
+            this.buttonLoadFile.Name = "buttonLoadFile";
+            this.buttonLoadFile.Size = new System.Drawing.Size(185, 23);
+            this.buttonLoadFile.TabIndex = 3;
+            this.buttonLoadFile.Text = "Загрузить из файла...";
+            this.buttonLoadFile.UseVisualStyleBackColor = true;
+            this.buttonLoadFile.Click += new System.EventHandler(this.buttonLoadFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 675);
+            this.Controls.Add(this.buttonLoadFile);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textBoxMain);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Художник";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxMain;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button buttonLoadFile;
+    }
+}
+
---
+++ CSharp_Work15_PainterWithForms/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CSharp_Work15_PainterWithForms
+{
+    public partial class Form1 : Form
+    {
+        private static int _size1;
+        private static int _size2;
+        private static string _ss;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawMap(Graphics g)
+        {
+            Pen p = new Pen (Color.Black, 1);
+            for (int i = 0; i < _size1; i++)
+            {
+                for (int j = 0; j < _size2; j++)
+                {
+                    g.DrawRectangle(p, i * 50, j * 50, 50, 50);
+                }
+            }
+        }
+
+        public void DrawRectangles(int x1, int y1, int x2, int y2)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            SolidBrush b = new SolidBrush(Color.Green);
+            g.FillRectangle(b, x1, y1, x2, y2);
+        }
+
+        
+
+        private void Cleaning()
+        {
+            _ss = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.DarkGray);
+            labelResult.Text = "Свободных клеток: ";
+            textBoxMain.Text = "";
+        }
+
+        private void MainCode(string ss)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            string[] words = ss.Split(' ', '\n', '\r');
+            int[] newWords = new int[words.Length];
+
+            for (int i = 0, j = 0; i < words.Length; i++)
+            {
+                bool flag = int.TryParse(words[i], out var number);
+                if (flag)
+                {
+                    newWords[j] = number;
+                    j++;
+                }
+            }
+            _size1 = newWords[0];
+            _size2 = newWords[1];
+            
+            var arr = new int[_size1, _size2];
+            var num = newWords[2];
+            int value = 3;
+
+            while (num != 0)
+            {
+                var x1 = newWords[value];
+                var y1 = newWords[value + 1];
+                var x2 = newWords[value + 2];
+                var y2 = newWords[value + 3];
+                DrawRectangles(x1 * 50, y1 * 50, (x2 - x1) * 50, (y2 - y1) * 50);
+                for (int j = x1; j < x2; j++)
+                {
+                    for (int k = y1; k < y2; k++)
+                    {
+                        arr[j, k] = 1;
+                    }
+                }
+                value += 4;
+                num--;
+            }
+
+            DrawMap(g);
+
+            int count = 0;
+            for (int i = 0; i < _size1; i++)
+            {
+                for (int j = 0; j < _size2; j++)
+                {
+                    if (arr[i, j] == 0)
+                        count++;
+                }
+            }
+
+            labelResult.Text = "Свободных клеток: " + count;
+        }
+
+        
+        
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            _ss = "5 5\r\n2\r\n1 1 3 3\r\n2 2 4 4";
+            textBoxMain.Text = _ss;
+            MainCode(_ss);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            _ss = "6 7\r\n3\r\n0 0 5 5\r\n1 1 4 4\r\n2 2 3 3";
+            textBoxMain.Text = _ss;
+            MainCode(_ss);
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _ss = textBoxMain.Text;
+                MainCode(_ss);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            Cleaning();
+            string filePath = "";
+            try
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.InitialDirectory = @"С:\";
+                if (fd.ShowDialog() == DialogResult.OK)
+                    filePath = fd.FileName;
+                _ss = System.IO.File.ReadAllText(filePath);
+                textBoxMain.Text = _ss;
+                MainCode(_ss);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Cleaning();
+        }
+
+
+    }
+}
